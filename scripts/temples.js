c80e20e2d064ea75431cb1b374f8b61d5f54f3c2
tendayimazvomba <tgmazvomba@gmail.com>
2026-07-21 04:45:05 +0200

    update w02

---
+++ scripts/temples.js
@@ -0,0 +1,18 @@
+const menuButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+menuButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+
+    if (navigation.classList.contains("open")) {
+        menuButton.textContent = "❌";
+    } else {
+        menuButton.textContent = "Ξ";
+    }
+});
+
+const currentYear = new Date().getFullYear();
+document.querySelector("#year").textContent = currentYear;
+
+document.querySelector("#lastModified").textContent = 'Last Modified: ${document.lastModified}';
+
